6232d7768619e9eaba3de98757e080e700cfef40
Steven <stevendawn98@gmail.com>
2022-03-26 13:26:38 -0400

    still trying to get js text to show up

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h1 = document.createElement("h1");
-h1.textContent = "This is my personal website!";
-document.querySelector("body").appendChild(h1);
+const h3 = document.createElement("h3");
+h3.textContent = "This is my personal website!";
+document.querySelector("body").appendChild(h3);
